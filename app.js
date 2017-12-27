6c24d25569da60d683c7531cc02d98436b83a90b
thegodfather904 <arscialo@gmail.com>
2017-12-26 21:56:57 -0500

    skills square close functionality working for the first skill

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-$(function(){
+$(function () {
   addClickEvents();
 });
 
-function addClickEvents(){
+function addClickEvents() {
   menuClick();
 }
 
-function menuClick(){
-  $('.menu-button').click(function(){
+function menuClick() {
+  $('.menu-button').click(function () {
 
     var menu = $('.menu');
     var menuButton = $('.menu-button');
 
-    if(menuButton.hasClass('menu-button-open')){
+    if (menuButton.hasClass('menu-button-open')) {
       menuButton.removeClass('menu-button-open');
       menu.fadeOut();
-    }
-    else{
+    } else {
       menuButton.addClass('menu-button-open');
       menu.fadeIn();
     }
@@ -24,18 +23,18 @@ function menuClick(){
   });
 }
 
-function skillsSquareClick(element){
+function skillsSquareClick(element) {
   $(element).addClass('active-skill');
 
-  $('.skills-section-inner .skills-column').children().each(function(index){
-    if(!$(this).hasClass('active-skill')){
+  $('.skills-section-inner .skills-column').children().each(function (index) {
+    if (!$(this).hasClass('active-skill')) {
       $(this).delay(100 * index).animate({
         opacity: 0
-      })
+      });
     }
   });
 
-  setTimeout(function(){
+  setTimeout(function () {
     $('.skills-section-inner').addClass('skill-is-active');
     $(element).animate({
       width: '100%',
@@ -44,5 +43,37 @@ function skillsSquareClick(element){
     $(element).find('.skills-square-content').fadeOut();
     $(element).find('.skills-square-inner-hidden').fadeIn();
   }, 1000);
-
 }
+
+$(".skills-close-container").click(function (event) {
+
+  var skillsSquare = $(this).closest('.skills-square');
+  skillsSquare.find('.skills-square-inner-hidden').fadeOut();
+
+  skillsSquare.animate({
+    width: '240px',
+    height: '240px',
+  }, 500);
+  skillsSquare.css({
+    overflow: 'initial'
+  });
+  
+  setTimeout(function() {
+    skillsSquare.removeClass('active-skill');
+    skillsSquare.find('.skills-square-content').fadeIn();
+  }, 500);
+
+  setTimeout(function() {
+    $('.skills-section-inner .skills-column').children().each(function (index) {
+      if (!$(this).hasClass('active-skill')) {
+        $(this).delay(100 * index).animate({
+          opacity: 1.0
+        });
+      }
+    });
+  
+    $('.skills-section-inner').removeClass('skill-is-active');
+  }, 500);
+  
+  event.stopPropagation();
+});
