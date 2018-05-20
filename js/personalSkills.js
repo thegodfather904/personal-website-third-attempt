29d3b2018ab8dd6c2caed0aff595721bed1c8bca
Scialo, Tony <tony_scialo@csx.com>
2018-05-19 21:00:24 -0400

    personal skills section kind of layed out correctly

--- js/personalSkills.js
+++ js/personalSkills.js
@@ -1,18 +1,17 @@
-$('#personalSkillCreative').mouseenter(function (e) {
-    $('.overlay').css("display", "flex")
-        .hide()
-        .fadeIn();;
-    var img = $('.img');
-    img.css({
-        position: "absolute",
-        left: e.pageX - 20,
-        top: e.pageY - 20
-    });
-    img.fadeIn();
-    // $('body').append('<div class="overlay">TRESTY</div>');
-});
-
-// $('.img').mouseleave(function (e) {
-//     $(this).fadeOut();
-//     $('.overlay').fadeOut();
+// $('#personalSkillCreative').mouseenter(function (e) {
+//     $('.overlay').css("display", "flex")
+//         .hide()
+//         .fadeIn();
+//     var img = $('.img');
+//     img.css({
+//         position: "absolute",
+//         left: e.pageX - 20,
+//         top: e.pageY - 20
+//     });
+//     img.fadeIn();
 // });
+
+$('.img').mouseleave(function (e) {
+    $(this).fadeOut();
+    $('.overlay').fadeOut();
+});
