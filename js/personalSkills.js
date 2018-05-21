7dd8a6760d26ec073f5c36cc421095f663800630
Scialo, Tony <tony_scialo@csx.com>
2018-05-20 20:59:29 -0400

    updated personal skills section AGAIN to new layout with card based approach

--- js/personalSkills.js
+++ js/personalSkills.js
@@ -1,15 +1,15 @@
-// $('#personalSkillCreative').mouseenter(function (e) {
-//     $('.overlay').css("display", "flex")
-//         .hide()
-//         .fadeIn();
-//     var img = $('.img');
-//     img.css({
-//         position: "absolute",
-//         left: e.pageX - 20,
-//         top: e.pageY - 20
-//     });
-//     img.fadeIn();
-// });
+$('#personalSkillCreative').mouseenter(function (e) {
+    $('.overlay').css("display", "flex")
+        .hide()
+        .fadeIn();
+    var img = $('.img');
+    img.css({
+        position: "absolute",
+        left: e.pageX - 20,
+        top: e.pageY - 20
+    });
+    img.fadeIn();
+});
 
 $('.img').mouseleave(function (e) {
     $(this).fadeOut();
