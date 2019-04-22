811a33753cb195dde0d77e21ad6d3f42a695a630
Scialo, Tony <tony_scialo@csx.com>
2019-04-22 10:49:08 -0400

    fixed stars going off screen

--- js/particle.js
+++ js/particle.js
@@ -1,6 +1,6 @@
 var radius = 2;
 
-$(function(){
+$(function () {
     var diminsionInterval = generateParticelForId('alternateDiminsionStar');
     generateParticelForId('dividerCanvas');
 });
@@ -17,69 +17,70 @@ function generateParticelForId(id) {
     let particles = setUpParticles(canvasWidth, canvasHeight);
 
     let speed = 50;
-    draw = setInterval(function(){
+    draw = setInterval(function () {
         update(particles, ctx, canvasWidth, canvasHeight);
     }, speed);
 
     return draw;
 }
 
-function update(particles, ctx, canvasWidth, canvasHeight){
+function update(particles, ctx, canvasWidth, canvasHeight) {
     drawParticles(particles, ctx, canvasWidth, canvasHeight);
 }
 
-function setUpParticles(canvasWidth, canvasHeight){
+function setUpParticles(canvasWidth, canvasHeight) {
     let particleArray = [];
-    for(var i = 0; i < 250; i++)
+    for (var i = 0; i < 250; i++)
         particleArray[i] = new randomParticle(canvasWidth, canvasHeight);
     return particleArray;
 }
 
-function drawParticles(particles, ctx, canvasWidth, canvasHeight){
+function drawParticles(particles, ctx, canvasWidth, canvasHeight) {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-    for(var i = 0; i < particles.length; i++){
-        if(!particles[i].shootingStar)
+    for (var i = 0; i < particles.length; i++) {
+        if (!particles[i].shootingStar)
             drawOneStroke(ctx, particles, i);
         else
             drawShootingStar(ctx, particles, i);
 
         //if off the screen, create a new particle and add it to that spot
-        if(particles[i].x > canvasWidth || particles[i].y < 0 )
+        if (particles[i].x > canvasWidth || particles[i].y < 0)
             particles[i] = new randomParticleAtStart(canvasHeight);
     }
 }
 
-function drawOneStroke(ctx, particles, i){
-        ctx.beginPath();
-        ctx.arc(particles[i].x, particles[i].y, particles[i].radius, 0, Math.PI * 2, true);
-        ctx.fillStyle = "rgba(255, 255, 255, " + particles[i].opacity + ")";
+function drawOneStroke(ctx, particles, i) {
+    ctx.beginPath();
+    ctx.arc(particles[i].x, particles[i].y, particles[i].radius, 0, Math.PI * 2, true);
+    ctx.fillStyle = "rgba(255, 255, 255, " + particles[i].opacity + ")";
 
-        ctx.fill();
-        particles[i].x = particles[i].x + 0.75;
-        particles[i].y = particles[i].y - 0.25;
+    ctx.fill();
+    particles[i].x = particles[i].x + 0.75;
+    // particles[i].y = particles[i].y - 0.25;
+    particles[i].y = particles[i].y - 0.05;
 }
 
-function drawShootingStar(ctx, particles, i){
+function drawShootingStar(ctx, particles, i) {
     ctx.beginPath();
     ctx.lineWidth = 3;
     ctx.strokeStyle = 'white';
     ctx.moveTo(particles[i].x, particles[i].y);
     particles[i].x = particles[i].x + 45;
     particles[i].y = particles[i].y - 10;
-    ctx.lineTo(particles[i].x , particles[i].y);
+    ctx.lineTo(particles[i].x, particles[i].y);
     ctx.stroke();
 }
 
-function randomParticle(canvasWidth, canvasHeight){
+function randomParticle(canvasWidth, canvasHeight) {
     this.x = Math.random() * canvasWidth;
     this.y = Math.random() * canvasHeight;
     this.radius = Math.floor(Math.random() * radius + 1);
     this.shootingStar = false;
-    this.opacity = Math.random().toFixed(2);    
+    this.opacity = Math.random().toFixed(2);
 }
 
-function randomParticleAtStart(canvasHeight){
+function randomParticleAtStart(canvasHeight) {
     this.x = 0;
     this.y = Math.random() * canvasHeight;
     this.radius = Math.floor(Math.random() * radius + 1);
@@ -87,13 +88,9 @@ function randomParticleAtStart(canvasHeight){
     this.opacity = Math.random().toFixed(2);
 }
 
-function shootingStar(seed){
-    if(Math.random() > seed)
+function shootingStar(seed) {
+    if (Math.random() > seed)
         return true;
     else
         return false;
 }
-
-
-
-
